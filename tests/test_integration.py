e55937cb3aade3ffbbd53b2ea02a75eafa846720
bshev <bshevits@gmail.com>
2026-05-09 18:03:06 -0700

    testing

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,6 +3,9 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 import asyncio
 import main
 
+"""
+These tests are only used to test if the code can actually communicate with the thermostat.
+"""
 
 def test_send_email():
     main.send_email("pytest integration test — email is working.", subject="Carrier Thermostat Email Test")
